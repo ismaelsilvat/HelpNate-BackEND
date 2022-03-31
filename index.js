ab0b59fb7d19cef42d4dbab5643fb85b85133d23
joaovs0 <ismaelsktcore9898@hotmail.com>
2022-03-31 17:49:07 -0300

    Updatin some queries

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req,file,cb) => {
+        cb(null, Date.now()+'-'+file.originalname);
+    }
+});
+
+const upload = multer({storage});
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.get("/usuarios", async(req,res) =>{
     try {
@@ -34,6 +48,14 @@ app.get("/usuarios/:id", async(req,res) =>{
     }
 });
 
+app.post("/imagemPerfil", upload.single('img'), async(req,res) =>{
+    try {               
+        await pool.query('UPDATE USUARIO SET FOTO_PERFIL = $1 WHERE IDUSUARIO = $2',[req.file.filename, req.body.id]);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
 app.get("/anuncios/:id", async(req,res) =>{
     try {
         const { id } = req.params;
@@ -55,9 +77,10 @@ app.post("/usuarioIncompleto", async(req,res) =>{
 
 app.post("/usuarioCompleto", async(req,res) =>{
     try {
-        await pool.query("INSERT INTO USUARIO(NOME, SOBRENOME, NASCIMENTO, EMAIL, SENHA, TELEFONE, CEP, CIDADE, ESTADO, BIOGRAFIA) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)" ,
+        let novoUsuario = await pool.query("INSERT INTO USUARIO(NOME, SOBRENOME, NASCIMENTO, EMAIL, SENHA, TELEFONE, CEP, CIDADE, ESTADO, BIOGRAFIA) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING IDUSUARIO;" ,
         [req.body.nome, req.body.sobrenome, req.body.nascimento, req.body.email, req.body.senha, req.body.telefone,
             req.body.cep, req.body.cidade, req.body.estado, req.body.biografia]);
+        res.json(novoUsuario.rows)
     } catch (err) {
         console.error(err.message);
     }
